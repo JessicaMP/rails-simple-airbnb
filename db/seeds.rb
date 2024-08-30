069989fc12a788ed8e1097549730bd438aa4d210
Jessica Manturano <jessica95.mp@gmail.com>
2024-08-30 15:30:27 -0500

    advance crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+4.times do
+  Flat.create!(
+    name: Faker::Commerce.department(max: 5),
+    address: Faker::Address.full_address,
+    description: Faker::Demographic.race,
+    price_per_night: Faker::Number.between(from: 50, to: 200),
+    number_of_guests: Faker::Number.between(from: 1, to: 5)
+  )
+end
